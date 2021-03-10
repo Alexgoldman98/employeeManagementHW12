e503f8e765ce2a18ef7ba9a40eb4ccb9caaa3c83
Alexandra <Alexgoldman98@gmail.com>
2021-03-09 19:34:53 -0500

    committing updates to all files

--- server.js
+++ server.js
@@ -1,41 +1,90 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-
-  // Your port, if not 3306
-  port: 3306,
-
-  // Your username
-  user: 'root',
-
-  // Be sure to update with your own MySQL password!
-  password: 'Password!Haha',
-  database: 'employee_db',
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  connection.end();
-});
+const { listenerCount } = require('events');
+const db = require( 'connection.js' )('employee_db','localhost')
 
 //ask questions
 
-inquirer.prompt([
-    /* Pass your questions in here */
-  ])
-inquirer.then(answers => {
-    // Use user feedback for... whatever!!
-  })
-inquirer.catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+async function askQuestions() {
+  const answers = await inquirer.prompt([
+    {
+      name: 'firstQ',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View All Employees By Department',
+        'View All Employees By Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager',
+        'Remove Manager',
+        'View all Roles',
+      ]
     }
-  });
+  ])
+  
+  if (answers.firstQ === 'View All Employees'){
+    viewAllEmployees()
+  } else if(answers.firstQ === 'View All Employees By Department'){
+    byDepartment()
+  } else if(answers.firstQ === 'View All Employees By Manager'){
+    byManager()
+  } else if(answers.firstQ === 'Add Employee'){
+    addEmployee()
+  } else if (answers.firstQ === 'Remove Employee'){
+    removeEmployee()
+  } else if (answers.firstQ === 'Update Employee Role'){
+    updateEmployee()
+  } else if(answers.firstQ === 'Update Employee Manager'){
+    updateManager()
+  } else if(answers.firstQ === 'Remove Manager'){
+    removeManager()
+  } else if(answers.firstQ === 'View all Roles'){
+    viewRoles()
+  } else{
+    console.log('Failed')
+  }
+};
+
+function viewAllEmployees(){
+  const allEmp = await db.query('SELECT')
+};
+
+function byDepartment(){
+
+};
+
+function byManager(){
+
+};
+
+function addEmployee(){
+
+};
+
+function removeEmployee(){
+
+};
+
+function updateEmployee(){
+
+};
+
+function updateManager(){
+
+};
+
+function removeManager(){
+
+};
+
+function viewRoles(){
+
+};
+
+askQuestions()
 
 //add departments, roles, employees,
 
